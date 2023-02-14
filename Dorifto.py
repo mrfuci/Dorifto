3fbb470614692909fb6647abe9af67c7162ec3e7
Jan <nypljan51@gmail.com>
2023-02-14 20:48:36 +0100

    Racer
    auto se dá normálně řídit

--- Dorifto.py
+++ Dorifto.py
@@ -1,84 +1,107 @@
-import sys
 import pygame
-
-pygame.init()
+import time
+import math
+from  tools import blit_rotate_center
 
 logo = pygame.image.load("logo.PNG")
 pygame.display.set_icon(logo)
 
-ROZLISENI_X = 800
-ROZLISENI_Y = 600
+GROUND = pygame.image.load("background.png")
+TRACK = pygame.image.load("Track.png")
+CAR = pygame.image.load("Lidl_car.png")
+
+WIDTH, HEIGHT = GROUND.get_width(), GROUND.get_height()
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Dorifto")
+
 FPS = 60
-CERNA_BARVA = (0, 0, 0)
-BILA_BARVA = (255, 255, 255)
-CERVENA_BARVA = (255, 0 , 0 )
-ZELENA_BARVA = ( 0, 255 , 0 )
-MODRA_BARVA = ( 0 , 0 , 255 )
 
-velikost = 25
-pozice_x = (ROZLISENI_X - velikost) / 2
-pozice_y = (ROZLISENI_Y - velikost) / 2
-rychlost = 10 
 
-#class AbstractCar:
-#    def __init__(self ,max_vel, rotation_vel):
-#        self.max_vel = max_vel
-#        self.vel = 0
-#       self.rotation_vel = rotation_vel
-#        self.angle = 0
+class AbstractCar:   
+    def __init__(self ,max_vel, rotation_vel):
+        self.img = self.IMG
+        self.max_vel = max_vel
+        self.vel = 0
+        self.rotation_vel = rotation_vel
+        self.angle = 0
+        self.x , self.y = self.START_POS
+<<<<<<< Updated upstream
+=======
+        self.acceleration = 0.25
+>>>>>>> Stashed changes
         
-#    def rotate(self, left=False , right=False):
-#        if left:
-#            self.angle += self.rotation_vel
-#        elif right:
-#            self.angle -= self.rotation_vel
+    def rotate(self, left=False , right=False):
+        if left:
+            self.angle += self.rotation_vel
+        elif right:
+            self.angle -= self.rotation_vel
+            
+    def draw(self,win):
+        blit_rotate_center(win, self.img, (self.x, self.y), self.angle)
+<<<<<<< Updated upstream
+
+=======
+    
+    def move_forward(self):
+        self.vel = min(self.vel + self.acceleration, self.max_vel)
+        self.move()
         
-hodiny = pygame.time.Clock()
-
-car = pygame.image.load("Lidl_car.png")
-BACKGROUND = pygame.image.load("background.png")
-
-okno = pygame.display.set_mode((ROZLISENI_X, ROZLISENI_Y))
-pygame.display.set_caption("Dorifo")
-
-
-while True:
-    for udalost in pygame.event.get():
-        if udalost.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-    klavesy = pygame.key.get_pressed()
+    def move(self):
+        radians = math.radians(self.angle)
+        vertical = math.cos(radians) * self.vel
+        horizontal = math.sin(radians) * self.vel
     
-    if klavesy[pygame.K_ESCAPE]:
-        pygame.quit()
-        sys.exit()
-
-
-    if klavesy[pygame.K_DOWN]:
-        pozice_y += rychlost
-    if klavesy[pygame.K_UP]:
-        pozice_y -= rychlost
-    if klavesy[pygame.K_LEFT]:
-       car = pygame.transform.rotate(car, 5)
-       pozice_x -= rychlost
-    if klavesy[pygame.K_RIGHT]:
-       car = pygame.transform.rotate(car, -5)
-       pozice_x += rychlost
+        self.y -= vertical
+        self.x -= horizontal
+        
+    def reduce_speed(self):
+        self.vel = max(self.vel - self.acceleration / 2, 0)
+        self.move()
     
-    if pozice_x > 800 - 50:
-        pozice_x = 800 - 50
-    if pozice_y > 600 - 50:
-        pozice_y = 600 - 50
-    if pozice_x < 0:
-        pozice_x = 0
-    if pozice_y < 0:
-        pozice_y = 0
+>>>>>>> Stashed changes
+class PlayerCar(AbstractCar):
+    IMG = CAR
+    START_POS = (50,50)
     
-    class platform(pygame.sprite.Sprite):
-    
-        okno.fill(BILA_BARVA)
-        okno.blit(car, (pozice_x, pozice_y))
-                
+def draw(win, images, player_car):
+    for img, pos in images:
+        win.blit(img, pos)
+   
+    player_car.draw(win)
     pygame.display.update()
-    hodiny.tick(FPS)
+ 
+run = True
+clock = pygame.time.Clock()
+images = [(GROUND, (0,0)), (TRACK, (0,0))]
+player_car = PlayerCar(4,4)
+
+while run:
+    clock.tick(FPS)
+    
+    draw(WIN, images, player_car)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            break
+<<<<<<< Updated upstream
+=======
+    
+    keys = pygame.key.get_pressed()
+    moved = False 
+    
+    if keys[pygame.K_a]:
+        player_car.rotate(left=True )
+    if keys[pygame.K_d]:
+        player_car.rotate(right=True)
+    if keys[pygame.K_w]:
+        moved = True 
+        player_car.move_forward()
+        
+    if not moved:
+        player_car.reduce_speed()
+>>>>>>> Stashed changes
+
+
+clock.tick(FPS)
+pygame.QUIT
