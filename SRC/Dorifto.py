9131fade285b3fc9b7a7e187d69547c03123edca
Jan <nypljan51@gmail.com>
2023-02-28 18:40:04 +0100

    začátek překopávání kódu
    ne úplná překopávka kódu

--- SRC/Dorifto.py
+++ SRC/Dorifto.py
@@ -3,9 +3,12 @@ import time
 import math
 from  tools import blit_rotate_center
 
+#cosmmetics
 logo = pygame.image.load("logo.PNG")
 pygame.display.set_icon(logo)
+pygame.display.set_caption("D for Drift")
 
+#Grafics
 GROUND = pygame.image.load("background.png")
 GROUND_MASK = pygame.image.load("background_mask.png")
 
@@ -14,27 +17,32 @@ TRACK_BORDER = pygame.image.load("Track_Border.png")
 
 TRACK_BORDER_MASK = pygame.mask.from_surface(TRACK_BORDER)
 
-CAR = pygame.image.load("Lidl_car.png")
+Car_image = pygame.image.load("Lidl_car.png")
 
+#res n fps
 X_RES = 800
 Y_RES = 600
 
 WIDTH, HEIGHT = GROUND.get_width(), GROUND.get_height()
 WIN = pygame.display.set_mode((X_RES, Y_RES))
-pygame.display.set_caption("D for Dorifto")
 
 FPS = 120
 
+#the car it self
 
 class AbstractCar:   
     def __init__(self ,max_vel, rotation_vel):
         self.img = self.IMG
         self.max_vel = max_vel
         self.vel = 0
+        self.speed = 0.5
         self.rotation_vel = rotation_vel
         self.angle = 180
-        self.x , self.y = self.START_POS
+        self.x = 53
+        self.y = 23
         self.acceleration = 0.25
+        self.drift = False
+        self.drift_distance = 0
  
     def rotate(self, left=False , right=False):
         if left:
@@ -68,7 +76,7 @@ class AbstractCar:
         return bp
         
 class PlayerCar(AbstractCar):
-    IMG = CAR
+    IMG = Car_image
     START_POS = (53,23)
     
     def reduce_speed(self):
@@ -85,11 +93,12 @@ def draw(win, images, player_car):
    
     player_car.draw(win)
     pygame.display.update()
- 
+
 run = True
 clock = pygame.time.Clock()
 images = [(GROUND, (0,0)), (TRACK, (0,0))]
 player_car = PlayerCar(4.5,4.5)
+
 
 while run:
     clock.tick(FPS)
@@ -103,27 +112,36 @@ while run:
       
     if player_car.collide(TRACK_BORDER_MASK) != None :
         player_car.bounce()
-        
+
+
 #    if player_car.collide(GROUND_MASK) != None :
 #        pygame.quit
+car = AbstractCar(image=Car_image)
 
+while True:
     keys = pygame.key.get_pressed()
     moved = False 
     
-    if keys[pygame.K_LEFT]:
-        player_car.rotate(left=True )
-    if keys[pygame.K_RIGHT]:
-        player_car.rotate(right=True)
-    if keys[pygame.K_UP]:
-        moved = True 
-        player_car.move_forward()
-    if keys[pygame.K_DOWN]:
-        moved = True
-        player_car.move_backward()
-        
-    if not moved:
-        player_car.reduce_speed()
-    
+    if (keys[pygame.K_SPACE] and keys[pygame.K_a] or keys[pygame.K_d]) or (car.speed > 6 and keys[pygame.K_a] or keys[pygame.K_d]) and not keys[pygame.K_s]:
+        if keys[pygame.K_a] and CAR.drift_distance < 50:
+            car.drift_distance += 1
+        elif keys[pygame.K_d] and car.drift_distance > -50:
+            car.drift_distance -= 1
+    else:
+        if car.drift_distance > 0:
+            car.drift_distance -= 1
+            if car.speed > 1:
+                car.speed -= 0.1
+            if car.drift_distance < 0:
+                car.drift_distance = 0
+        elif car.drift_distance < 0:
+            car.drift_distance += 1
+            if car.speed > 1:
+                car.speed -= 0.1
+
+            if car.drift_distance > 0:
+                car.drift_distance = 0
+
     if keys[pygame.K_ESCAPE]:
             pygame.quit()
              
